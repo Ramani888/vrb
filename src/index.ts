7b6b3682ac51ce52dea59f324efb498e9f77ff78
Divyeshrzeusint <divyeshr@zeusint.com>
2025-03-25 21:21:39 +0530

    update main file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
-import express, { Request, Response } from "express";
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+// import routes from './routes/routes'
+import cors from 'cors'; // Change to ES module import
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3010;
+const mongoUri = process.env.DATABASE_URL || 'mongodb+srv://CVLCluster1:Ramani%407258@atlascluster.g9ls9b9.mongodb.net/VR_Fashion_Temp';
 
-app.use(express.json());
+mongoose.connect(mongoUri);
+const database = mongoose.connection;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from TypeScript!");
+database.on('error', (error: any) => {
+  console.log(error);
 });
 
-const PORT = process.env.PORT || 3010;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+database.once('connected', () => {
+  console.log('Database Connected');
+});
+
+app.use(cors());
+app.use(express.json());
+// app.use('/api', routes);
+app.get('/', (req, res) => {
+  res.json("server working....");
+});
+
+app.get('*', (req, res) => {
+  res.json("API route not found");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
